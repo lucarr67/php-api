840663780de45ac66c2ab1c7beea1294e29531c6
lucarr67 <lucarr67@hotmail.com>
2025-04-12 07:32:19 +0200

    Update get_clienti.php

--- get_clienti.php
+++ get_clienti.php
@@ -12,12 +12,20 @@ if ($conn->connect_error) {
     die(json_encode(["status" => "error", "message" => "Connessione fallita: " . $conn->connect_error]));
 }
 
-// Query per ottenere i dati
-$sql = "SELECT * FROM clifor where obsoleto = 0 order by RagSoc1";
+// Recupera il parametro 'codice' dalla query string
+$codice = isset($_GET['codice']) ? $conn->real_escape_string($_GET['codice']) : null;
+
+// Costruzione query dinamica
+if ($codice) {
+    $sql = "SELECT * FROM clifor WHERE obsoleto = 0 AND codice = '$codice' ORDER BY RagSoc1";
+} else {
+    $sql = "SELECT * FROM clifor WHERE obsoleto = 0 ORDER BY RagSoc1";
+}
+
 $result = $conn->query($sql);
 
 $data = [];
-if ($result->num_rows > 0) {
+if ($result && $result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $data[] = $row;
     }
@@ -27,3 +35,4 @@ echo json_encode(["status" => "success", "data" => $data]);
 
 $conn->close();
 ?>
+
