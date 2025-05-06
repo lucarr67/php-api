dcb91b145d7e893e8567fcec9a643796fc1abce5
lucarr67 <lucarr67@hotmail.com>
2025-05-06 21:39:57 +0200

    Update ins_bolle_test.php

--- ins_bolle_test.php
+++ ins_bolle_test.php
@@ -12,15 +12,33 @@ if ($conn->connect_error) {
     die(json_encode(["status" => "error", "message" => "Connessione fallita: " . $conn->connect_error]));
 }
 
+// Ricevi i dati tramite POST
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (isset($data["serie"]) && 
+    isset($data["numero"]) && 
+    isset($data["riga"]) && 
+    isset($data["data"]) ) {
+
+    // Estrai i dati
+    $serie = $data["serie"];
+    $numero = $data["numero"];
+    $riga = $data["riga"];
+    $data = $data["data"];
+ 
     // Prepara la query SQL per inserire i dati
     $sql = "INSERT INTO bolle (serie, numero, riga, data)
-            VALUES ('1', '1', '1', '1')";
+            VALUES ('$serie', '$numero', '$riga', '$data')";
 
     if ($conn->query($sql) === TRUE) {
         echo json_encode(["status" => "success", "message" => "Riga bolla inserita correttamente."]);
     } else {
         echo json_encode(["status" => "error", "message" => "Errore nell'inserimento: " . $conn->error]);
     }
+
+} else {
+    echo json_encode(["status" => "error", "message" => "Dati incompleti."]);
+}
 
 $conn->close();
 ?>
