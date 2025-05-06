7ccd9fed38a4eabedabe9b48049e57bb0b391d9c
lucarr67 <lucarr67@hotmail.com>
2025-05-06 21:29:45 +0200

    Add files via upload

---
+++ ins_bolle_test.php
@@ -0,0 +1,32 @@
+<?php
+$host = "mysql-15764f65-albalatte-mysql.b.aivencloud.com";
+$port = "25265";
+$dbname = "bolle";
+$username = "avnadmin";
+$password = getenv('AIVEN_PASSWORD');
+
+// Connessione al database
+$conn = new mysqli($host, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die(json_encode(["status" => "error", "message" => "Connessione fallita: " . $conn->connect_error]));
+}
+
+
+
+    // Prepara la query SQL per inserire i dati
+    $sql = "INSERT INTO bolle (serie, numero, riga, data)
+            VALUES ('1', '1', '1', '1')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo json_encode(["status" => "success", "message" => "Riga bolla inserita correttamente."]);
+    } else {
+        echo json_encode(["status" => "error", "message" => "Errore nell'inserimento: " . $conn->error]);
+    }
+
+} else {
+    echo json_encode(["status" => "error", "message" => "Dati incompleti."]);
+}
+
+$conn->close();
+?>
