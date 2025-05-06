13f3d67de3b65089d6ccd252f2d1bfde9b4dae1c
lucarr67 <lucarr67@hotmail.com>
2025-05-06 21:56:08 +0200

    Update ins_bolle_test.php

--- ins_bolle_test.php
+++ ins_bolle_test.php
@@ -1,6 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
 
 $host = "mysql-15764f65-albalatte-mysql.b.aivencloud.com";
 $port = "25265";
